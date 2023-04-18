ea64fdd95b03576a60eb8479d41bc73c8974dc66
Waduzzaman <waduzzaman@gmail.com>
2023-04-18 10:46:53 -0400

    db changed

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 ﻿//Development configuration options
 //To sign the session identifier, use a secret string
 module.exports = {
-    db: 'mongodb://127.0.0.1:27017/hospital-db',
+    db: 'mongodb+srv://comp308_group6_project:2X0kcgIpuZUhuhzF@cluster0.e62f7dn.mongodb.net/test',
     sessionSecret: 'developmentSessionSecret',
     secretKey: 'real_secret'
 };
